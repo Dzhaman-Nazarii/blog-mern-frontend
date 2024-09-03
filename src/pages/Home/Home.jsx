9404ad25a2172f406a8f66130e4f96da13864f58
Nazarii <dzhaman.nazar2003@gmail.com>
2024-09-03 11:15:03 +0300

    add fetch posts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,21 @@ import React, { useEffect } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
-import axios from "../../service/axios.js"
 import { Post } from "../../components/Post/Post";
 import { TagsBlock } from "../../components/TagsBlock/TagsBlock";
 import { CommentsBlock } from "../../components/CommentsBlock/CommentsBlock";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../../redux/slices/posts.js";
 
 export const Home = () => {
+	const dispatch = useDispatch();
+	const {posts} = useSelector(state => state.posts);
+	const isPostsLoading = posts.status === "loading";
+
 	useEffect(() => {
-		axios.get("/posts")
-	}, []);
+		dispatch(fetchPosts());
+	}, [dispatch]);
+
 	return (
 		<>
 			<Tabs
@@ -26,21 +32,20 @@ export const Home = () => {
 				<Grid
 					xs={8}
 					item>
-					{[...Array(1)].map(() => (
+					{(isPostsLoading ? [...Array(1)] : posts.items).map((obj, index) => isPostsLoading ? (<Post key={index} isLoading={true}/>) : (
 						<Post
-							key={1}
-							id={1}
-							title="Roast the code #1 | Rock Paper Scissors"
+							key={obj._id}
+							id={obj._id}
+							title={obj.title}
 							imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
 							user={{
-								avatarUrl:
-									"https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-								fullName: "Keff",
+								avatarUrl: obj.user.avatarURL,
+								fullName: obj.user.name,
 							}}
-							createdAt={"12 june 2022 year"}
-							viewsCount={150}
-							commentsCount={3}
-							tags={["react", "fun", "typescript"]}
+							createdAt={obj.createdAt}
+							viewsCount={obj.viewsCount}
+							commentsCount={obj.commentsCount}
+							tags={obj.tags}
 							isEditable
 						/>
 					))}
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,4 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../service/axios";
+
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+	const { data } = await axios.get("/posts");
+	return data;
+});
+
+// export const fetchTags = createAsyncThunk("tags/fetchTags", async () => {
+// 	const { data } = await axios.get("/tags");
+// 	return data;
+// });
 
 const initialState = {
 	posts: {
@@ -14,7 +25,21 @@ const initialState = {
 const postsSlice = createSlice({
 	name: "posts",
 	initialState,
-	reducer: {},
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchPosts.pending, (state) => {
+				state.posts.status = "loading";
+			})
+			.addCase(fetchPosts.fulfilled, (state, action) => {
+				state.posts.items = action.payload;
+				state.posts.status = "loaded";
+			})
+			.addCase(fetchPosts.rejected, (state) => {
+				state.posts.items = [];
+				state.posts.status = "error";
+			});
+	},
 });
 
 export const postsReducer = postsSlice.reducer;
